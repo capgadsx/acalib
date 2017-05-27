cbed0d94d2a58e31027ae0079b796b5b8470eb77
Carlos Andres Ponce <cponce@alumnos.inf.utfsm.cl>
2017-05-27 13:29:39 -0400

    Run checkCube in all files in folder

--- IndexingDask.py
+++ IndexingDask.py
@@ -7,9 +7,7 @@ indexing.config['PRECISION'] = 0.01
 indexing.config['SAMPLES'] = 100
 folder = '/mnt/nfs/cycle0fits_copia/cycle0fits/'
 files = glob.glob(folder+'*.fits')
-print files
-'''checkResults = indexing.runChecks(files)
+checkResults = indexing.runChecks(files)
 for indx, result in enumerate(checkResults):
     if result[0]:
         print(os.path.basename(files[indx])+' failed because of '+result[1])
-'''
